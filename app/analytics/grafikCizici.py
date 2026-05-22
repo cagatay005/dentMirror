0c0c4c09b77281844c7e7fcbe5f4666242f9aecc
Mustafa Cagatay Ozdem <mustafaozdem40@gmail.com>
2026-05-23 00:05:15 +0300

    Grafik hızlandırması

--- app/analytics/grafikCizici.py
+++ app/analytics/grafikCizici.py
@@ -1,22 +1,15 @@
-import sys
-import matplotlib
-
-# Eğer işletim sistemi Windows ise bu motoru kullan, Linux ise kendi haline bırak
-if sys.platform == 'win32':
-    matplotlib.use('TkAgg') 
-
-import matplotlib.pyplot as plt
-
-# test sonuclarini gorsel grafiklere donusturen sinif
 class grafikCizici:
     def __init__(self, veriCercevesi):
         self.df = veriCercevesi
 
-    # zaman icindeki y koordinati degisimini ve hata anlarini cizer
     def hataGrafigiCiz(self):
         if self.df.empty:
             print("Grafik çizilecek veri bulunamadı.")
             return
+
+        # SİHİRLİ DOKUNUŞ: Matplotlib'i sadece bu butona basıldığında yüklüyoruz.
+        # Bu sayede uygulamanın ilk açılış hızı roket gibi olacak!
+        import matplotlib.pyplot as plt
 
         plt.figure(figsize=(8, 5))
         
